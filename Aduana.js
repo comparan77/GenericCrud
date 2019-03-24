8d44978f6d951cb436b909589041d0340aba3616
gil <comparan77@gmail.com>
2019-03-24 05:05:38 -0600

    ready

---
+++ Aduana.js
@@ -0,0 +1,8 @@
+function Aduana() {
+    this.Id = 0;
+    this.Codigo = '';
+    this.Nombre = '';
+    this.IsActive = false;
+};
+
+module.exports = Aduana;
---
+++ TableMng.js
@@ -0,0 +1,68 @@
+function TableMng() {
+
+        //Global element references
+
+        //Define default options
+        var defaults = {
+            objMng: null,
+            conn: null
+        }
+
+        // Create options by extending defaults with the passed in arugments
+        if (arguments[0] && typeof arguments[0] === "object") {
+            this.options = extendDefaults(defaults, arguments[0]);
+        }
+
+        //private methods
+
+        //public methods
+        TableMng.prototype.ObjMng = function(objMng = null) {
+            if(objMng == null)
+                return this.options.objMng;
+            else 
+                this.options.objMng = objMng;
+        }
+
+        TableMng.prototype.Action = function(action, callback, tran = null) {
+            var _ = this;
+            var opcion = 0;
+            switch (action) {
+                case 'get':
+                    opcion = 1;
+                    break;
+                case 'add':
+                    opcion = 2;
+                    break;
+                case 'udt':
+                    opcion = 3;
+                    break;
+                case 'dlt':
+                    opcion = 4;
+                    break;
+                default:
+                    break;
+            }
+
+            _.options.objMng.fillParameters(opcion);
+            var values = Object.values(_.options.objMng.Params);
+            var params = '(' + Object.values(_.options.objMng.Params).fill('?') + ')';
+            var query = _.options.conn.query('call sp_' + _.options.objMng.TableName + params, values, function(err, res, fields) {
+                if(err) throw err;
+                if(callback) callback(res[0]);
+            });
+        }
+
+        // Utility method to extend defaults with user options
+        function extendDefaults(source, properties) {
+        var property;
+        for (property in properties) {
+            if (properties.hasOwnProperty(property)) {
+                source[property] = properties[property];
+            }
+        }
+        return source;
+}
+
+    };
+
+module.exports = TableMng;
