6d08feddf95b00cd1d712db1ea61589039643a65
Gilberto Cruz Comparan <comparan77@gmail.com>
2019-03-24 13:23:33 -0600

    ready

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
-function fillModelClass() {
+const fs = require('fs');
 
-    var sbClass = "";
-    var sbClassMng = "";
-    var sbFields = "\t\t#region Campos";
-    var sbFieldsMng = "\t\t#region Campos";
-    var sbProperties = "\t\t#region Propiedades";
-    var sbPropertiesMng = "\t\t#region Propiedades";
-    var sbConstructor = "\t\t#region Constructores";
-    var sbConstructorMng = "\t\t#region Constructores";
-    var sbAddParameters = "\t\tprotected override void addParameters(int opcion)";
+function fillMng(obj, callback) {
 
-    
+    var strMng = "var BaseMng = require('./basemng.js'); \n\
+    \n\
+    function " + obj.Name + "Mng (o, lst = null) { \n\
+    \tBaseMng.call(this, o, '" + obj.Name + "', lst) \n\
+    \n\
+    \tthis.Params = { \n\
+    \t\tOption: 0, \n\
+    \t\tId: 0, \n\
+    \t\tCodigo: '', \n\
+    \t\tNombre: '' \n\
+    \t} \n\
+    \n\
+    }; \n\
+    \n\
+    " + obj.Name + "Mng.prototype = Object.create(BaseMng.prototype); \n\
+    " + obj.Name + "Mng.prototype.constructor = " + obj.Name + "Mng; \n\
+    \n\
+    " + obj.Name + "Mng.prototype.fillParameters = function(option) { \n\
+    \tthis.Params.Option = option; \n\
+    \tthis.Params.Id = this.obj.Id; \n\
+    \tthis.Params.Codigo = this.obj.Codigo; \n\
+    \tthis.Params.Nombre = this.obj.Nombre; \n\
+    } \n\
+    \n\
+    module.exports = " + obj.Name + "Mng;";
+
+    var data = {
+        Str: strMng,
+        Name: obj.Name
+    }
+
+    if(callback) callback(data);
 }
 
-fillModelClass();
+fillMng({
+    Name: 'Usuario'
+}, function(data) {
+    fs.writeFile("./model/" + data.Name + ".js", data.Str, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    }); 
+});
