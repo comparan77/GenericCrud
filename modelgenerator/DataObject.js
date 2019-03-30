4f45da647a402e2351834dc0e1bc4736b9221570
Gilberto Cruz Comparan <comparan77@gmail.com>
2019-03-29 19:52:01 -0600

    add DataObject class

---
+++ modelgenerator/DataObject.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = function() {
+    this.FieldName;
+    this.FieldType;
+    this.IsNull;
+    this.IsFieldLogicalDelete;
+    this.Character_maximun_length;
+    this.IsPk;
+    this.IsPkAI;
+}
--- modelgenerator/ManageData.js
+++ modelgenerator/ManageData.js
@@ -6,12 +6,15 @@ function ManageData() {
     this._database;
     this._tableName;
     this._isLogigalDelete;
+    this._primaryKey;
+    this._lst = [];
 
     //Define default options
     var defaults = {
         conn: null,
         database: '',
-        table: ''
+        table: '',
+        dataobject: null
     }
 
     // Create options by extending defaults with the passed in arugments
@@ -28,12 +31,37 @@ function ManageData() {
 
     // Private Methods
     function fillDataObjects() {
-        this.options.conn.query("select column_name, data_type, case is_nullable when 'YES' then 1 else 0 end is_nullable, character_maximum_length, case column_key when 'PRI' then 1 else 0 end IsPk, case extra when 'auto_increment' then 1 else 0 end IsPkAI from information_schema.columns where table_schema = '" + this._database + "' and table_name='" + this._tableName + "';", function(err, res, fields) {
+        var _ = this;
+        var qry = "select column_name, data_type, case is_nullable when 'YES' then 1 else 0 end is_nullable, character_maximum_length, case column_key when 'PRI' then 1 else 0 end IsPk, case extra when 'auto_increment' then 1 else 0 end IsPkAI from information_schema.columns where table_schema = '" + this._database + "' and table_name='" + this._tableName + "';";
+        //console.log(qry);
+        _.options.conn.query(qry, function(err, result, fields) {
                 if(err) throw err;
                 
                 this._isLogigalDelete = false;
-                console.log(JSON.stringify(res));
-
+                Object.keys(result).forEach(function(key) {
+                    var row = result[key];
+                    var o = new _.options.dataobject();
+                    o.FieldName = row.column_name.toLowerCase();
+                    o.IsFieldLogicalDelete = false;
+                    if(o.FieldName == 'isactive') {
+                        o.FieldName = "IsActive";
+                        o.IsFieldLogicalDelete = true;
+                        _._isLogigalDelete = true;
+                    }
+                    o.FieldType = row.data_type;
+                    o.IsNull = row.is_nullable == 0 ? false : true;
+                    o.Character_maximun_length = 0;
+                    if(row.character_maximum_length!=null) {
+                        o.Character_maximun_length = row.character_maximum_length;
+                    }
+                    o.IsPk = row.IsPk;
+                    o.IsPkAI = row.IsPkAI;
+                    if(o.IsPk) {
+                        _._primaryKey = o;
+                    }
+                    _._lst.push(o);
+                  }); 
+                  console.log(_._primaryKey);
             });
     }
 
@@ -50,9 +78,11 @@ function ManageData() {
 }
 
 var pool = require('../db.js');
+var dataobject = require('./DataObject.js')
 var o = new ManageData({
     conn: pool,
     database: 'dbcasc_qa',
-    table: 'aduana'
+    table: 'aduana',
+    dataobject: dataobject
 });
 o.Init();
