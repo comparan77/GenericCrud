54edd62ef982ddb0273344861f401a3a52b97dbb
Gilberto <comparan77@gmail.com>
2019-07-06 11:21:55 -0500

    ready

--- index.js
+++ index.js
@@ -1,113 +1,84 @@
 const fs = require('fs');
 
-// function fillMng(obj, callback) {
-
-//     var strMng = "var BaseMng = require('./basemng.js'); \n\
-//     \n\
-//     function " + obj.Name + "Mng (o, lst = null) { \n\
-//     \tBaseMng.call(this, o, '" + obj.Name + "', lst) \n\
-//     \n\
-//     \tthis.Params = { \n\
-//     \t\tOption: 0, \n\
-//     \t\tId: 0, \n\
-//     \t\tCodigo: '', \n\
-//     \t\tNombre: '' \n\
-//     \t} \n\
-//     \n\
-//     }; \n\
-//     \n\
-//     " + obj.Name + "Mng.prototype = Object.create(BaseMng.prototype); \n\
-//     " + obj.Name + "Mng.prototype.constructor = " + obj.Name + "Mng; \n\
-//     \n\
-//     " + obj.Name + "Mng.prototype.fillParameters = function(option) { \n\
-//     \tthis.Params.Option = option; \n\
-//     \tthis.Params.Id = this.obj.Id; \n\
-//     \tthis.Params.Codigo = this.obj.Codigo; \n\
-//     \tthis.Params.Nombre = this.obj.Nombre; \n\
-//     } \n\
-//     \n\
-//     module.exports = " + obj.Name + "Mng;";
-
-//     var data = {
-//         Str: strMng,
-//         Name: obj.Name
-//     }
-
-//     if(callback) callback(data);
-// }
-
-// fillMng({
-//     Name: 'Usuario'
-// }, function(data) {
-//     fs.writeFile("./model/" + data.Name + ".js", data.Str, function(err) {
-//         if(err) {
-//             return console.log(err);
-//         }
-    
-//         console.log("The file was saved!");
-//     }); 
-// });
-
 var pool = require('./db.js');
 var dataobject = require('./modelgenerator/DataObject.js')
 var Common = require('../common/Common.js');
 
 var ManageData = require('./modelgenerator/ManageData');
-var o = new ManageData({
-    conn: pool,
-    database: '4_Test',
-    table: 'mercancia_vendor',
-    dataobject: dataobject,
-    common: Common
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-o.Init(function(result) {
-    
-    fs.writeFile("/home/gil/Develop/node/4CAD_Model/model/" + result.tblName + ".js", result.strBean, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    });
+rl.question('Nombre de la tabla ', (tblName) => {
+  // TODO: Log the answer in a database
+  // console.log(`Thank you for your valuable feedback: ${answer}`);
 
-    fs.writeFile("/home/gil/Develop/node/4CAD_Model/model/" + result.tblName + "Mng.js", result.strMng, function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    });
+  var o = new ManageData({
+      conn: pool,
+      database: '4_Test',
+      table: tblName,
+      dataobject: dataobject,
+      common: Common
+  });
 
-    pool.getConnection(function(err, conn) {
-        if (err) throw err;
+  console.log(JSON.stringify(o));
 
-        conn.beginTransaction(function(err) {
-            if(err) { throw err; }
-            conn.query(result.strSqlH, function(error, results, fields) {
-                if (error) {
-                    return conn.rollback(function() {
-                      throw error;
-                    });
-                }
-            
-                conn.query(result.strSql, function(error, results, fields) {
-                if (error) {
-                    return conn.rollback(function() {
-                      throw error;
-                    });
-                  }
-                  conn.commit(function(err) {
-                    if (err) {
+  rl.close();
+});
+
+
+function GenericCode() {
+  o.Init(function(result) {
+      
+      fs.writeFile("/home/gil/Develop/node/4CAD_Model/model/" + result.tblName + ".js", result.strBean, function(err) {
+          if(err) {
+              return console.log(err);
+          }
+          console.log("The file was saved!");
+      });
+
+      fs.writeFile("/home/gil/Develop/node/4CAD_Model/model/" + result.tblName + "Mng.js", result.strMng, function(err) {
+          if(err) {
+              return console.log(err);
+          }
+          console.log("The file was saved!");
+      });
+
+      pool.getConnection(function(err, conn) {
+          if (err) throw err;
+
+          conn.beginTransaction(function(err) {
+              if(err) { throw err; }
+              conn.query(result.strSqlH, function(error, results, fields) {
+                  if (error) {
                       return conn.rollback(function() {
-                        throw err;
+                        throw error;
+                      });
+                  }
+              
+                  conn.query(result.strSql, function(error, results, fields) {
+                  if (error) {
+                      return conn.rollback(function() {
+                        throw error;
                       });
                     }
-                    console.log('success procedure!');
-                });
-                });
-            });
-        });
-        conn.release();
-    //fin get connection
-    })
-
-});
+                    conn.commit(function(err) {
+                      if (err) {
+                        return conn.rollback(function() {
+                          throw err;
+                        });
+                      }
+                      console.log('success procedure!');
+                  });
+                  });
+              });
+          });
+          conn.release();
+      //fin get connection
+      })
+  });
+}
